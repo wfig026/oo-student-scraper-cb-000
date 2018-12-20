34439523662cc8c90f23abbf41a79d19827267b8
Will Figueiredo <will@wfigueiredo.com>
2018-12-20 19:02:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student|
+      dude = self.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
